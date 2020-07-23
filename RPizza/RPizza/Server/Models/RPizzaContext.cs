ddfa306154d8696405b3725cc6f3ba20813a0ca5
malom <malom@LAPTOP-KGH25DSQ>
2020-07-22 22:18:16 -0500

    Configuración de Contexto de Datos

---
+++ RPizza/RPizza/Server/Models/RPizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPizza.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPizza.Server.Models
+{
+    public class RPizzaContext : DbContext
+    {
+        public RPizzaContext(DbContextOptions<RPizzaContext> options): base(options)
+        {
+        }
+        public DbSet<PizzaSpecial> Special { get; set; }
+
+    }
+}
--- RPizza/RPizza/Server/Startup.cs
+++ RPizza/RPizza/Server/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using RPizza.Server.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RPizza.Server
 {
@@ -22,7 +24,10 @@ namespace RPizza.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<RPizzaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("RPizzaContext"));
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
