b9764fa31212c7777528fd56b9c2bfaba65d8eb9
malom <malom@LAPTOP-KGH25DSQ>
2020-07-26 18:17:57 -0500

    Topping Solicitar

---
+++ RPizza/RPizza/Server/Controllers/ToppingsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RPizza.Server.Models;
+using RPizza.Shared;
+namespace RPizza.Server.Controllers
+{
+    [Route("toppings")]
+    [ApiController]
+    public class ToppingsController : ControllerBase
+    {
+        private readonly RPizzaContext Context;
+
+        public ToppingsController(RPizzaContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Topping>>> GetTopping()
+        {
+            return await Context.Toppings
+                .OrderByDescending(t => t.Name).ToListAsync();
+        }
+    }
+}
---
+++ RPizza/RPizza/Shared/OrderWithStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPizza.Shared
+{
+    public class OrderWithStatus
+    {
+        public readonly static TimeSpan PreparationDuration
+            = TimeSpan.FromSeconds(20);
+
+        public readonly static TimeSpan DeliveryDuration
+            = TimeSpan.FromSeconds(20);
+
+        public Order Order { get; set; }
+
+        public string StatusText { get; set; }
+
+        //public List<Marker> MapMarkers { get; set; }
+
+
+    }
+}
