db70ef1896ab458c257cb9be502465acba405ca2
malom <malom@LAPTOP-Q1R4DMD6>
2020-08-12 22:30:47 -0500

    Actualización de Clases Dinamicas Modelo Sigleton

--- RPizza/RPizza/Client/Program.cs
+++ RPizza/RPizza/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RPizza.Client.Services;
 
 namespace RPizza.Client
 {
@@ -18,6 +19,8 @@ namespace RPizza.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<OrderState>();
 
             await builder.Build().RunAsync();
         }
---
+++ RPizza/RPizza/Client/Services/OrderState.cs
@@ -0,0 +1,49 @@
+﻿using RPizza.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPizza.Client.Services
+{
+    public class OrderState //AppState
+    {
+        public Pizza ConfiguringPizza { get; private set; }
+        public bool ShowingConfigureGialog { get; private set; }
+        public Order Order { get; private set; } = new Order();
+
+        public void ShowConfigurePizzaGialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new Pizza()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+            ShowingConfigureGialog = true;
+        }
+
+        public void CancelConfigurePizzaGialog()
+        {
+            ConfiguringPizza = null;
+            ShowingConfigureGialog = false;
+        }
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            Order.Pizzas.Add(ConfiguringPizza);
+            ConfiguringPizza = null;
+            ShowingConfigureGialog = false;
+        }
+        public void RemoveConfiguredPizza(Pizza pizza)
+        {
+            Order.Pizzas.Remove(pizza);
+        }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
+    }
+}
