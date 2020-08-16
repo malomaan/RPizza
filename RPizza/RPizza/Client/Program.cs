b1b2681919847fb0ccb44f16dce8fe88b75d3973
malom <malom@LAPTOP-Q1R4DMD6>
2020-08-15 21:14:50 -0500

    Implementando seguridad

--- RPizza/RPizza/Client/Program.cs
+++ RPizza/RPizza/Client/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using RPizza.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+
 
 namespace RPizza.Client
 {
@@ -21,7 +23,11 @@ namespace RPizza.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddScoped<OrderState>();
+            
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
 
+            builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
             await builder.Build().RunAsync();
         }
     }
---
+++ RPizza/RPizza/Client/Services/ServerAutenticationStateProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+namespace RPizza.Client.Services
+{
+    public class ServerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var Claim = new Claim(ClaimTypes.Name, "Usuario False");
+            var Identity = new ClaimsIdentity(new[] { Claim }, "serverauth");
+            return new AuthenticationState(new ClaimsPrincipal(Identity));
+        }
+    }
+
+}
